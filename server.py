f3bf4575a498bdcddb2cc19858c91a54136940b1
André Pascal Beyer <beyeran@gmail.com>
2017-01-14 19:51:21 +0100

    changed server for multiple sites

--- server.py
+++ server.py
@@ -1,39 +1,39 @@
 # models
-from tfidf_models import SVMModel
+from ai.tfidf_models import SVMModel
 
 # server
-from flask import Flask
-from flask_restplus import Resource, Api, reqparse
+from flask import Flask, request, render_template, jsonify
 
 # obtain model
-svm = SVMModel('data/train.tsv') # initialize model (path not really needed)
-svm.fromdisk('models/svm.model') # load pretrained model
+svm = SVMModel('ai/data/train.tsv') # initialize model (path not really needed)
+svm.fromdisk('ai/models/svm.model') # load pretrained model
 
 app = Flask(__name__)
-api = Api(app)
 
-class ClassifySentiment(Resource):
-    def post(self):
-        try:
-            parser = reqparse.RequestParser()
-            parser.add_argument('sentiment', type=str, help='The sentiment to be classified.')
-            args = parser.parse_args()
+# api
+@app.route('/sentiment', methods=['POST'])
+def sentiment_classification():
+    try:
+        _sentiment = request.form['sentiment']
 
-            _sentiment = args['sentiment']
+        if _sentiment == None:
+            return jsonify({'status': 100, 'message': 'invalid form data'})
 
-            if _sentiment == None:
-                return {'status': 100, 'message': 'invalid form data'}
+        classification = int(svm.classify(_sentiment))
+        sentiments = ['positive', 'somewhat positive',
+                      'neutral',
+                      'somewhat negative', 'negative']
 
-            classification = svm.classify(_sentiment)
+        return jsonify({'status': 200, 'classification': sentiments[classification]})
 
-            sentiments = ['positive', 'somewhat positive', 'neutral', 'somewhat negative', 'negative']
-            
-            return {'status': 200, 'classification': sentiments[classification]}
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
-        except Exception as e:
-            return {'error': str(e)}
 
-api.add_resource(ClassifySentiment, "/sentiment")
+@app.route("/")
+def main():
+    return 'hello'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
