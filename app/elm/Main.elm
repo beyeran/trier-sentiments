92b8af147187eb665397030062829b3267ae13eb
André Pascal Beyer <beyeran@gmail.com>
2017-01-16 23:07:32 +0100

    first elm draft

--- app/elm/Main.elm
+++ app/elm/Main.elm
@@ -1,62 +1,88 @@
 module Main exposing (main)
 
-import Header exposing (header)
+-- import Header exposing (header)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Html.App exposing (..)
 import Http
+import HttpBuilder exposing (..)
 import Json.Decode as Decode exposing (..)
+import Json.Decode.Pipeline as JsonPipeline exposing (decode, required)
+import Json.Decode.Extra exposing ((|:))
 import Json.Encode as Encode exposing (..)
 import Task
 
 main =
-    header
+    Html.App.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
+subscriptions : Model -> Sub Msg
+subscriptions model = Sub.none
 
--- Messages
-type Msg
-    = SendSentiment
-    | GetSentiment String
-    | FetchFail Http.Error
+-- view
+view : Model -> Html Msg
+view model =
+    div []
+        [ h2 [] [text "just a damn test"]
+        , button [ onClick Submit ] [ text "Classify" ]
+        , br [] []
+        , p [] [ text model.classification ]
+        ]
 
-{-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model =
-    case msg of
-        SendSentiment ->
-            model ! [sendSentiment model.sentiment]
-        GetSentiment value ->
-            ( { model | classification = (toString value) }, Cmd.none )
-        FetchFail _ ->
-            ( { model | sentiment = "", classification = "" }, Cmd.none )
--}
-{-
-  Model
--}
+-- model
 type alias Model =
     { sentiment : String
     , classification : String
     }
 
-
-{-
-  Update
--}
-encodeSentiment : Model -> Encode.Value
-encodeSentiment model =
-    Encode.object [ ("sentiment", Encode.string model.sentiment ) ]
-
-sendSentiment : Model -> Task.Task Http.Error String
-sendSentiment model =
-    { verb = "POST"
-    , headers = [ ("Content-Type", "application/json") ]
-    , url = "http://127.0.0.1:5000/sentiment"
-    , body = Http.string <| Encode.encode 0 <| encodeSentiment model
+type alias JSONResponse =
+    { classification : String
+    , status : Int
     }
-    |> Http.send Http.defaultSettings
-    |> Http.fromJson tokenDecoder
 
+init : (Model, Cmd Msg)
+init = (Model "what a terrible movie" "", Cmd.none )
 
-tokenDecoder : Decode.Decoder String
-tokenDecoder = "classification" := Decode.string
+-- update
+type Msg = Submit
+         | FetchSucceed JSONResponse
+         | FetchFail Http.Error
 
-fetchSentimentCmd : Model -> Cmd Msg
-fetchSentimentCmd model =
-    Task.perform GetSentiment (sendSentiment model)
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+    case msg of
+        Submit ->
+            (model, fetchSentiment model.sentiment)
+
+        FetchSucceed response->
+            (Model model.sentiment response.classification, Cmd.none)
+
+        FetchFail error ->
+            (model, Cmd.none)
+
+-- http
+jsonify : String -> Http.Body
+jsonify str =
+    let
+        json = Encode.encode 0 <| Encode.object [("sentiment", Encode.string str)]
+    in
+        Http.string json
+
+decodeJson : Decode.Decoder JSONResponse
+decodeJson =
+    decode JSONResponse
+        |> JsonPipeline.required "sentiment" Decode.string
+        |> JsonPipeline.required "status" Decode.int
+
+decodeSentiment : String -> Platform.Task Http.Error JSONResponse
+decodeSentiment sentiment =
+    Http.post decodeJson "http://127.0.0.1:5000/sentiment" (jsonify sentiment)
+
+fetchSentiment : String -> Cmd Msg
+fetchSentiment sentiment =
+    Task.perform FetchFail FetchSucceed (decodeSentiment sentiment)
