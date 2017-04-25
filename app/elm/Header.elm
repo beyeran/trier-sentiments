0bb89b8c732b8d9852503c641e10d88ff3fdd80a
Andre Pascal Beyer <beyer@uni-trier.de>
2017-04-25 13:53:27 +0200

    finished design draft

--- app/elm/Header.elm
+++ app/elm/Header.elm
@@ -5,12 +5,8 @@ import Html.Attributes exposing (id, href)
 
 header =
     div [ id "content" ] [
-         div [ id "header" ] [
-              h1 [] [ text "Trier Sentiments" ]
+         div [ id "main" ] [
+              textarea [] [ text "Type here..." ]
              ],
-             div [ id "main" ] [
-                  textarea [] [ text "Type here..." ]
-                 ],
              div [ id "output" ] []
         ]
-    
